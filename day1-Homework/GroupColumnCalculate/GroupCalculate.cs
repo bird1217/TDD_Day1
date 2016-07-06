3a4aaf0cf6e4bd8b08f30d9fedd0ae997fb47162
ben.tseng <bird1217@gmail.com>
2016-07-06 15:54:44 +0800

    將參數由建構子傳入  改為呼叫方法時才指定.

--- day1-Homework/GroupColumnCalculate/GroupCalculate.cs
+++ day1-Homework/GroupColumnCalculate/GroupCalculate.cs
@@ -7,23 +7,11 @@ namespace GroupColumnCalculate
 {
     public class GroupCalculate
     {
-        private List<Order> _orderDts;
-        private int _groupCount;
-        private string _groupCoulumnName;
-
-
-        public GroupCalculate(List<Order> orderDts, int groupCount, string groupCoulumnName)
-        {
-            _orderDts = orderDts;
-            _groupCount = groupCount;
-            _groupCoulumnName = groupCoulumnName;
-        }
-
-        public List<int> GroupColumnCalculate()
+        public List<int> GroupColumnCalculate(List<Order> orderDts, int groupCount, string groupCoulumnName)
         {
             List<int> Subtotal = new List<int>();
             int dataIndex = 0;
-            var splitOrder = _orderDts.Skip(dataIndex * _groupCount).Take(_groupCount).ToList();
+            var splitOrder = orderDts.Skip(dataIndex * groupCount).Take(groupCount).ToList();
 
             while (splitOrder.Count > 0)
             {
@@ -31,14 +19,14 @@ namespace GroupColumnCalculate
                 foreach (var _orderItem in splitOrder)
                 {
                     //透過reflection抓出特定欄位值
-                    int actualValue = (int)_orderItem.GetType().GetProperty(this._groupCoulumnName).GetValue(_orderItem);
+                    int actualValue = (int)_orderItem.GetType().GetProperty(groupCoulumnName).GetValue(_orderItem);
                     subtotal += actualValue;
                 }
                 Subtotal.Add(subtotal);
 
                 //繼續擷取後面的群組資料,直到資料抓完
                 dataIndex++;
-                splitOrder = _orderDts.Skip(dataIndex * _groupCount).Take(_groupCount).ToList();
+                splitOrder = orderDts.Skip(dataIndex * groupCount).Take(groupCount).ToList();
             }
             return Subtotal;
         }
--- day1-Homework/GroupColumnCalculateTests/GroupCalculateTests.cs
+++ day1-Homework/GroupColumnCalculateTests/GroupCalculateTests.cs
@@ -18,10 +18,10 @@ namespace GroupColumnCalculate.Tests
             //Arrange            
             List<Order> Orders = this.GetOrderContext();
             List<int> _expceted = new List<int>() { 6, 15, 24, 21 };
-            GroupCalculate _target = new GroupCalculate(Orders, 3, "Id");
+            GroupCalculate _target = new GroupCalculate();
 
             //Act                                
-            List<int> _actual = _target.GroupColumnCalculate();
+            List<int> _actual = _target.GroupColumnCalculate(Orders, 3, "Id");
 
             //Assert
             _expceted.ToExpectedObject().ShouldEqual(_actual);
@@ -35,23 +35,31 @@ namespace GroupColumnCalculate.Tests
             List<Order> Orders = this.GetOrderContext();
             List<int> _expceted = new List<int>() { 50, 66, 60 };
 
-            GroupCalculate _target = new GroupCalculate(Orders, 4, "Revenue");
+            GroupCalculate _target = new GroupCalculate();
 
-            //Act                                
-            List<int> _actual = _target.GroupColumnCalculate();
+            //Act
+            List<int> _actual = _target.GroupColumnCalculate(Orders, 4, "Revenue");
 
             //Assert
             _expceted.ToExpectedObject().ShouldEqual(_actual);
         }
 
+
         List<Order> GetOrderContext()
         {
             List<Order> orderContext = new List<Order>();
 
-            for (int _index = 1; _index <= 11; _index++)
-            {
-                orderContext.Add(new Order() { Id = _index, Cost = _index, Revenue = _index + 10, SellPrice = _index + 20 });
-            }
+            orderContext.Add(new Order { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
+            orderContext.Add(new Order { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
+            orderContext.Add(new Order { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
+            orderContext.Add(new Order { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
+            orderContext.Add(new Order { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
+            orderContext.Add(new Order { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
+            orderContext.Add(new Order { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
+            orderContext.Add(new Order { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
+            orderContext.Add(new Order { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
+            orderContext.Add(new Order { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
+            orderContext.Add(new Order { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
             return orderContext;
         }
     }
